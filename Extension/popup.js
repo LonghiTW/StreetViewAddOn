f0c8f051661c4630f87bbcfb759d3f5ccb908b02
LonghiTW <165005419+LonghiTW@users.noreply.github.com>
2024-06-28 11:17:51 +0800

    feat: Replace button with toggle for main switch

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,15 +1,22 @@
 (async () => {
-	let btn = document.getElementById('open');
-
-	btn.innerHTML = 'Loading...';
-	let open = (await chrome.storage.sync.get('open')).open;
-	btn.innerText = open ? 'Close' : 'Open';
-	btn.onclick = async () => {
-		open = (await chrome.storage.sync.get('open')).open;
-		chrome.storage.sync.set({
-			open: !open,
-			opentab: (await chrome.storage.sync.get('opentab')).opentab,
-		});
-		btn.innerText = open ? 'Open' : 'Close';
-	};
+  document.addEventListener('DOMContentLoaded', async function () {
+    const SwitchStatus = document.getElementById('MainSwitch');
+  
+    // Get initial status
+    const { enabled } = await chrome.storage.sync.get('enabled');
+  
+    // Set checkbox status
+    SwitchStatus.checked = enabled;
+  
+    // Listen change of checkbox status
+    SwitchStatus.addEventListener('change', async () => {
+      const isChecked = SwitchStatus.checked;
+  
+      // Update status
+      await chrome.storage.sync.set({
+        enabled: isChecked,
+        opentab: (await chrome.storage.sync.get('opentab')).opentab,
+      });
+    });
+  });
 })();
